bcbb8edb722e53fe00775f6cf677ddc86b9b6d1f
RukshanDias <rukshandias002@gmail.com>
2023-06-28 09:44:57 +0530

    update Route with Controller

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { getGoals, setGoal, updateGoal, deleteGoal } = require("../controller/goalController");
 
-router.get("/", (req, res) => {
-    res.status(200).json({ message: "Get goals" });
-});
+router.get("/", getGoals);
 
-router.post("/", (req, res) => {
-    res.status(200).json({ message: "Set goals" });
-});
+router.post("/", setGoal);
+// router.route("/").get(getGoals).post(setGoal); <- both GET & POST in one line. only when endpoint is same.
 
-router.put("/:id", (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` });
-});
+router.put("/:id", updateGoal);
 
-router.delete("/:id", (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` });
-});
+router.delete("/:id", deleteGoal);
+// router.route("/:id").put(updateGoal).delete(deleteGoal);
 
 module.exports = router;
